2174744b9fcd8393096cd7a1b6c4421a82a2cbb3
Sanoj Punchihewa <sanoj.punchihewa@gmail.com>
2017-12-06 12:18:32 +0530

    added_btn_again

--- index.js
+++ index.js
@@ -631,9 +631,32 @@ app.post('/', function(request, res){
 
         let path = 'https://vast-peak-63221.herokuapp.com/' + data;        
 	  
-        var reply = {'messages': [{"type": 0, "speech": path}]}       
+        //var reply = {'messages': [{"type": 0, "speech": path}]}       
 
-        res.send(JSON.stringify(reply))
+        //res.send(JSON.stringify(reply))
+	      
+	 var reply = {
+              'data':{
+                "facebook": {
+                  "attachment": {
+                    "type": "template",
+                    "payload": {
+                      "template_type": "button",
+                      "text":'Full Timetable',
+                      "buttons": [{
+                        "type":"web_url",
+                        "url":`${path}`,
+                        "title":"Show"
+                      }]
+                    }
+                  }
+                }
+              }
+            }
+        //var reply = {'messages': [{"type": 0, "speech": path}]}
+
+        res.send(JSON.stringify(reply))     
+	      
       }else {
         res.send(JSON.stringify({'messages': [{"type": 0, "speech": err}]}))
       }
